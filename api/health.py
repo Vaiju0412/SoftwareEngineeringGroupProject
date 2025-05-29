96a728eb71b34702662ca29d19cb912c616d3ea8
22f3002815 <22f3002815@ds.study.iitm.ac.in>
2025-05-29 20:04:36 +0530

    Delete api directory

--- api/health.py
+++
@@ -1,8 +0,0 @@
-from flask_restx import Namespace, Resource
-
-api = Namespace('health', description='Health related operations')
-
-@api.route('/')
-class Health(Resource):
-    def get(self):
-        return {"message": "Health endpoint working!"}
